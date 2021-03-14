29de25dbad1f08917ba98a05cac98992a2fbebf4
GKuzzinshtern <72338984+GKuzzinshtern@users.noreply.github.com>
2021-03-14 12:27:19 +0200

    Перевод символов в верхний регистр(аналогto_upper)
    С клавиатуры необходимо ввести строку и перевести строку в верхний регист Например
    
    HelloWorld1This
    
    HELLOWORLD1THIS

---
+++ 3_3_ToUpper.cpp
@@ -0,0 +1,34 @@
+// Перевод символов в верхний регистр
+#include <iostream>
+
+char chGetHi(const unsigned char in_cuchSymbol);
+
+int main() {
+  constexpr size_t csizeMAXCHAR{256};
+  constexpr char cchENDOFSTRING{'\0'};
+  unsigned char auchInBuffer[csizeMAXCHAR]{};
+
+  std::cout << "Enter the string : ";
+  std::cin >> auchInBuffer; // without any checking
+
+  for (size_t sizeCharIndex = 0; sizeCharIndex < csizeMAXCHAR;
+       ++sizeCharIndex) {
+    if (*(auchInBuffer + sizeCharIndex) == cchENDOFSTRING) {
+      break;
+    }
+    *(auchInBuffer + sizeCharIndex) = chGetHi(*(auchInBuffer + sizeCharIndex));
+  }
+  std::cout << "To upper string: ";
+  std::cout << auchInBuffer << '\n';
+
+  return 0;
+}
+
+char chGetHi(const unsigned char in_cuchSymbol) {
+  constexpr unsigned char cuchDIFFERENCE{
+      0x20}; // difference between hi and low symbol in ASCII table
+  if ((in_cuchSymbol < 'a') || (in_cuchSymbol > 'z')) {
+    return in_cuchSymbol;
+  }
+  return (in_cuchSymbol - cuchDIFFERENCE);
+}
