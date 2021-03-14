bbcada463833bd718abc956f0725d286fb6af5ba
GKuzzinshtern <72338984+GKuzzinshtern@users.noreply.github.com>
2021-03-14 12:34:36 +0200

    Является ли символ числом(аналог is_digit)
    С клавиатуры необходимо ввести строку и проверить является ли символ числом. Например
    
    abfdc1k4l0
    
    is digit 1, 4, 0

---
+++ 3_4_IsDigit.cpp
@@ -0,0 +1,30 @@
+//Проверить является ли введеный символ числом
+#include <iostream>
+
+bool bIsDigit(const unsigned char cuchSymbol) {
+  return (cuchSymbol >= '0') || (cuchSymbol <= '9');
+}
+
+int main() {
+  constexpr size_t csizeMAXCHAR{256};
+  constexpr char cchENDOFSTRING{'\0'};
+  unsigned char auchInBuffer[csizeMAXCHAR]{};
+
+  std::cout << "Enter the string for digits extracting: ";
+  std::cin >> auchInBuffer; // without any checking
+
+  std::cout << "Digits are: ";
+  for (size_t sizeCharIndex = 0; sizeCharIndex < csizeMAXCHAR; ++sizeCharIndex) {
+    if (*(auchInBuffer + sizeCharIndex) == cchENDOFSTRING) {
+      break;
+    }
+    if (!bIsDigit(*(auchInBuffer + sizeCharIndex))) {
+      continue;
+    }
+    std::cout << *(auchInBuffer + sizeCharIndex) << ' ';
+  }
+  std::cout << '\n';
+  return 0;
+}
+
+
